7627681aaf79d3ba7942a6b118cc7e69cf91db8f
johnmachacek <72056701+johnmachacek@users.noreply.github.com>
2021-02-18 12:16:48 -0500

    Update ErrorCorrectingCode.py
    Adds documentation and fixes #4 to make constructor check length of codewords.

--- ErrorCorrectingCode.py
+++ ErrorCorrectingCode.py
@@ -1,11 +1,35 @@
 class ErrorCorrectingCode:
-  '''This class defines an error correcting code'''
+  '''
+  A class to represent  an error correcting code
+  
+  Attributes
+  ----------
+  code_words: A list of strings representing code words in the code
+  __n: The (common) length of each code word
+
+  Functions
+  ---------
+  size(): Returns the size of the code (i.e. the number of code words)
+  length(): Returns length of any (all) code word(s)
+  '''
   
   def __init__(self, C):
-    self.code_words = C
-  
+    '''
+    Creates an error correcting code.
+    
+    Parameters
+    ----------
+    C: a list of codewords each of which should be string of the same length
+    '''
+    m = len(C[0])
+    if all([len(w) == m for w in C]):
+      self.code_words = C
+      self.__n = m
+    else:
+      raise ValueError("Error: all code words must have the same length")
+
   def size(self):
-    return len(C)
+    return len(self.code_words)
   
   def length(self):
-    return len(C[0])
+    return self.__n
