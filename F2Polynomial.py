8b7121a4894e60fb2830587cd6636ae25b706c36
JacksonHR2000 <jacksonhrobertson@gmail.com>
2021-03-19 09:34:56 -0400

    Finished constructor for F2 Polynomial
    + Added constructor
    [need] Print statement

--- F2Polynomial.py
+++ F2Polynomial.py
@@ -1,36 +1,34 @@
 # exec(open("F2Polynomial.py").read())
-# Need to reset the p data member to private by using __.
+# Need to shift
 
 class F2Polynomial:
     # A class to handle F2 polynomial generation. An F2 polynomial is a list of either 1 or 0.
     # It can be thought of as any number mod(%) by 2.
 
-    def __init__(self, p):
+    def __init__(self, p):  # *** The convention is highest order bit on left, lowest order bit on the right. ***
         if type(p) is int:
             self.__p = p
 
-        # Need to flip the passed in list around. Its reading binary backwards right now
         elif type(p) is list:
-            # Calculate the value of a binary list, as an int.
+            # Calculate the value of a binary list, as a list.
 
             self.__p = 0    # Generate data member p. This will be an int representing the binary number
-            for i in p:     # Loop through the passed in list, and calculate the int representation
+            p.reverse()     # Reverse the list that was passed in.
+            for i in range(len(p)):     # Loop through the passed in list, and calculate the int representation
                 if p[i] == 1:
                     self.__p = pow(2, i) + self.__p
 
                 if p[i] > 1 or p[i] < 0:
-                    raise ValueError("A list must contain all 1s and 0s, since it is meant to represent a binary num")
-            print(self.__p)
+                    raise ValueError("A list must contain all 1s and 0s (t is meant to represent a binary number)")
 
-        # Not done
         elif type(p) is str:
-            # Calculate the value of a binary list, as an int.
+            # Calculate the value of a binary list, as a string.
 
             self.__p = 0    # Generate data member p. This will be an int representing the binary number
-            for i in p:     # Loop through the passed in list, and calculate the int representation
-                if p[i] == 1:
+            p = p[::-1]
+            for i in range(len(p)):     # Loop through the passed in list, and calculate the int representation
+                if int(p[i]) == 1:
                     self.__p = pow(2, i) + self.__p
 
-                if p[i] > 1 or p[i] < 0:
+                if int(p[i]) > 1 or int(p[i]) < 0:
                     raise ValueError("A list must contain all 1s and 0s, since it is meant to represent a binary num")
-
