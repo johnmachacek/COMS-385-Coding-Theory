00aa38713c081ebb7fabc835fd02bc78b280b57b
Ryan Irizarry <36823966+irizarryr@users.noreply.github.com>
2021-04-23 09:32:17 -0400

    Linear Code start
    Trying to get all the matrix work done and straightened out using Numpy

---
+++ linear_code.py
@@ -0,0 +1,27 @@
+# Linear Code class
+
+#preferred constructor pass in parity check matrix
+#check each list within llist  has same length
+import numpy as np
+import ErrorCorrectingCode
+
+#a = np.array([[1, 2, 3], [3, 4, 5]])
+#print(a)                    #[[1 2 3]
+                             #[3 4 5]]
+#print(type(a))              #<class 'numpy.ndarray'>
+
+
+class linear_code(ErrorCorrectingCode):
+
+    def __init__(self, l):
+
+        #Starting with only list of strings
+        if type(l) is list:
+            self.__l = np.ndarray(l)
+
+
+    def syndrome(parity, v):
+
+        v = "1101"
+        vList = list(v)
+        vMatrix = numpy.reshape(vList, (1, ))
